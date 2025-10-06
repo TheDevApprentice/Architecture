7c053d7e864ed18b7748b23758b2a0f5fd3ad29b
habric <80226527+TheDevApprentice@users.noreply.github.com>
2025-10-06 22:36:14 +0200

    Moving to next operation to do : making the cluster a empty shell to have databases on it

--- server/PostgreSQL/Patroni/init.sh
+++ server/PostgreSQL/Patroni/init.sh
@@ -6,25 +6,25 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$PATRONI_SUPERUSER_USERNAME" <<-EOSQL
     -- Create database if not exists
-    SELECT 'CREATE DATABASE ${POSTGRES_DB:-keycloak}'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB:-keycloak}')\gexec
+    -- SELECT 'CREATE DATABASE ${POSTGRES_DB:-keycloak}'
+    -- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB:-keycloak}')\gexec
     
     -- Create user if not exists
-    DO \$\$
-    BEGIN
-        IF NOT EXISTS (SELECT FROM pg_user WHERE usename = '${POSTGRES_USER:-user}') THEN
-            CREATE USER "${POSTGRES_USER:-user}" WITH PASSWORD '${POSTGRES_PASSWORD:-user}';
-        END IF;
-    END
-    \$\$;
+    --  DO \$\$
+    -- BEGIN
+    --     IF NOT EXISTS (SELECT FROM pg_user WHERE usename = '${POSTGRES_USER:-user}') THEN
+    --         CREATE USER "${POSTGRES_USER:-user}" WITH PASSWORD '${POSTGRES_PASSWORD:-user}';
+    --     END IF;
+    -- END
+    -- \$\$;
     
     -- Grant privileges
-    GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB:-keycloak}" TO "${POSTGRES_USER:-user}";
+    -- GRANT ALL PRIVILEGES ON DATABASE "${POSTGRES_DB:-keycloak}" TO "${POSTGRES_USER:-user}";
     
     -- Connect to the database and grant schema privileges
-    \c ${POSTGRES_DB:-keycloak}
-    GRANT ALL ON SCHEMA public TO "${POSTGRES_USER:-user}";
-    ALTER DATABASE "${POSTGRES_DB:-keycloak}" OWNER TO "${POSTGRES_USER:-user}";
+    -- \c ${POSTGRES_DB:-keycloak}
+    -- GRANT ALL ON SCHEMA public TO "${POSTGRES_USER:-user}";
+    -- ALTER DATABASE "${POSTGRES_DB:-keycloak}" OWNER TO "${POSTGRES_USER:-user}";
     
     -- Create replication user if not exists (pour monitoring style ProxySQL)
     DO \$\$
